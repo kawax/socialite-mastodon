2c5f26cee466fabf5797de46f8a55b5537abfe17
kawax <kawaxbiz@gmail.com>
2018-05-10 12:45:17 +0900

    defer

--- src/MastodonServiceProvider.php
+++ src/MastodonServiceProvider.php
@@ -13,7 +13,7 @@ class MastodonServiceProvider extends SocialiteServiceProvider
      *
      * @var bool
      */
-    protected $defer = true;
+    protected $defer = false;
 
     /**
      * Bootstrap the service provider.
